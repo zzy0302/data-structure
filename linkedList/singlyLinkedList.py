b7620eadbb51a92d456bff7e6b4aef72c393ac3c
zzy0302 <503652983@qq.com>
2020-11-16 17:08:26 +0800

    make linkedlist great again

--- linkedList/singlyLinkedList.py
+++ linkedList/singlyLinkedList.py
@@ -3,18 +3,21 @@ class singlyLinkedListNode():
         self.val = val
         self.next = next
 
-    def __str__(self):
-        return self.val
-
     def __next__(self):
-        return self.next
+        if self.next:
+            return self.next
+        else:
+            raise StopIteration
 
 
 class singlyLinkedList():
     def __init__(self, query=[]):
         start, end = 0, len(query)-1
         if end < 0:
-            raise IndexError
+            self.head = None
+            self.tail = None
+            self.length = 0
+            return
         linkListStart = singlyLinkedListNode(query[start])
         temp = linkListStart
         while start < end:
@@ -26,10 +29,12 @@ class singlyLinkedList():
         self.length = len(query)
 
     def __str__(self):
+        if self.isEmpty():
+            return ''
         a = ''
         node = self.head
         while(node.next):
-            a += str(node.val)+', '
+            a += str(node.val)+' -> '
             node = node.next
         a += str(node.val)
         return a
@@ -37,17 +42,41 @@ class singlyLinkedList():
     def __len__(self):
         return self.length
 
+    def __iter__(self):
+        self.current = self.head
+        return self
+
+    def __next__(self):
+        if self.current is not None:
+            temp = self.current
+            self.current = self.current.next
+            return temp
+        else:
+            self.current = self.head
+            raise StopIteration
+
+    def isEmpty(self):
+        return self.head == None
+
     def addFirst(self, value):
-        self.head = singlyLinkedListNode(value, self.head)
-        self.length += 1
+        if self.isEmpty():
+            self.head = singlyLinkedListNode(value, self.head)
+            self.tail = self.head
+            self.length += 1
+        else:
+            self.head = singlyLinkedListNode(value, self.head)
+            self.length += 1
 
     def addLast(self, value):
+        if self.isEmpty():
+            self.addFirst(value)
+            return
         self.tail.next = singlyLinkedListNode(value)
         self.length += 1
         self.tail = self.tail.next
 
     def insertAfter(self, target, value):
-        if self.head == None:
+        if self.isEmpty():
             return False
         node = self.head
         while node.val != target and node.next:
@@ -60,7 +89,7 @@ class singlyLinkedList():
             return True
 
     def insertBefore(self, target, value):
-        if self.head == None:
+        if self.isEmpty():
             return False
         prev, cur = self.head, self.head.next
         if prev.val == target:
@@ -76,7 +105,7 @@ class singlyLinkedList():
             return True
 
     def remove(self, target):
-        if self.head == None:
+        if self.isEmpty():
             return False
         if self.head.val == target:
             self.head = self.head.next
@@ -92,14 +121,40 @@ class singlyLinkedList():
             self.length -= 1
             return True
 
+    def reverse(self):
+        _head = self.head
+        _tail = self.tail
+        if self.isEmpty():
+            return False
+        if self.head.next == None:
+            return True
+        if self.length == 2:
+            _head.next = None
+            _tail.next = _head
+            self.head = _tail
+            self.tail = _head
+            return True
+        prev, cur, nxt = self.head, self.head.next, self.head.next.next
+        prev.next = None
+        # self.head, self.tail = self.head, self.tail
+        while nxt and nxt.next:
+            nxt, cur.next, prev, cur = nxt.next, prev, cur, nxt
+        cur.next = prev
+        nxt.next = cur
+        self.head = nxt
+        return True
+
 
 if __name__ == "__main__":
     a = singlyLinkedList([1, 2, 3, 4, 5, 6, 7, 8, 9])
     print(a)
     print(len(a))
     a.addFirst(123)
+    print(a)
+    print(len(a))
     a.addLast(456)
     print(a)
+    print(len(a))
     a.insertAfter(9, 10)
     print(a)
     print(len(a))
@@ -109,3 +164,7 @@ if __name__ == "__main__":
     a.remove(1)
     print(a)
     print(len(a))
+    a.reverse()
+    print(a)
+    for i in a:
+        print(i.val)
